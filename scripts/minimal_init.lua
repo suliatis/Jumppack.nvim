74d73e86887fa89fc518369b4d4c59904feb1a42
suliatis <suli.zakar.attila@gmail.com>
2025-08-30 14:21:39 +0200

    simplify minimal_init and test scripts

--- scripts/minimal_init.lua
+++ scripts/minimal_init.lua
@@ -1,51 +1,18 @@
--- Minimal init.lua for testing Jumppack plugin
--- This configuration provides the minimal setup needed to test the plugin
-
--- Clone 'mini.nvim' manually in a temporary directory for testing
 local path_package = vim.fn.stdpath('data') .. '/site'
 local mini_path = path_package .. '/pack/deps/start/mini.nvim'
-
 if not vim.loop.fs_stat(mini_path) then
-  vim.cmd('echo "Installing mini.nvim for tests..."')
+  vim.cmd('echo "Installing `mini.nvim`" | redraw')
   vim.fn.system({
     'git',
     'clone',
     '--filter=blob:none',
+    '--branch',
+    'stable',
     'https://github.com/echasnovski/mini.nvim',
     mini_path,
   })
+  vim.cmd('packadd mini.nvim | helptags ALL')
 end
-
--- Add mini.nvim to runtime path
-vim.cmd('set rtp+=' .. mini_path)
 
 -- Add current plugin to runtime path
-local plugin_path = vim.fn.fnamemodify(debug.getinfo(1, 'S').source:sub(2), ':h:h')
-vim.cmd('set rtp+=' .. plugin_path)
-
--- Setup mini.test
-require('mini.test').setup()
-
--- Load the plugin
-vim.cmd('runtime! plugin/**/*.lua')
-
--- Set up minimal vim options for testing
-vim.o.swapfile = false
-vim.o.backup = false
-vim.o.writebackup = false
-vim.o.hidden = true
-vim.o.shortmess = 'atIFWc' -- Added F, W, c to suppress more messages
-
--- Disable some features that might interfere with testing
-vim.o.updatetime = 10
-vim.o.cmdheight = 1
-vim.o.report = 99999 -- Don't report changes (suppresses "1L, 15B" messages)
-vim.o.verbose = 0 -- Suppress verbose messages
-
--- Enable termguicolors for proper highlighting in tests
-if vim.fn.has('termguicolors') == 1 then
-  vim.o.termguicolors = true
-end
-
--- Set up basic colorscheme
-vim.cmd('colorscheme default')
+vim.cmd([[let &rtp.=','.getcwd()]])
--- scripts/test.lua
+++ scripts/test.lua
@@ -1,27 +1,3 @@
--- Test runner script for Jumppack plugin
--- This script runs all tests using mini.test in headless mode
-
-local function run_tests()
-  -- Load the test file
-  local test_file = 'tests/test_jumppack.lua'
-  local test_set = dofile(test_file)
-
-  -- Run the tests
-  local result = require('mini.test').run(test_set, {
-    -- Configuration for test execution
-    execute = {
-      reporter = require('mini.test').gen_reporter.stdout({ group_depth = 2 }),
-      stop_on_error = false,
-    },
-  })
-
-  -- Exit with appropriate code
-  if result and (result.n_fail > 0 or result.n_error > 0) then
-    vim.cmd('cquit 1')
-  else
-    vim.cmd('qall')
-  end
-end
-
--- Schedule the test run to happen after initialization
-vim.schedule(run_tests)
+local MiniTest = require('mini.test')
+MiniTest.setup()
+MiniTest.run()
